c3bee442a9fa06e7cf42841fff075e84d4d52343
Josh Wufsus <108442458+jwuffy@users.noreply.github.com>
2025-11-02 14:22:28 -0500

    Eight Point Algorithm

---
+++ eightpointprecanned.m
@@ -0,0 +1,15 @@
+
+%test eight point algorithm
+
+% precanned version using previously carefully clicked points!
+
+
+%read in precanned points
+load savepoints.mat
+x1 = savx1; y1 = savy1; x2 = savx2; y2 = savy2;
+
+im = imread('oldmain6.jpg');
+im2 = imread('oldmain5.jpg');
+
+F = eightpoint(im,im2,[x1 y1],[x2 y2]);
+
